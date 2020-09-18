90740df1cc3f17583e9e51cf826a069b4417222b
danbulant <danbulant@gmail.com>
2020-09-18 21:54:47 +0200

    Implement updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
+
+if(!isDev) require('update-electron-app')()
 
 app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 
